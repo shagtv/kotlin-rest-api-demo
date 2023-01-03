041da2040b3ff85be46421bc4051582c2cacad1f
shagtv <shagtv@eyelinkmedia.com>
2023-01-03 01:31:37 +0000

    added a service and tests for it

---
+++ src/main/kotlin/com/shagtv/web/service/BankService.kt
@@ -0,0 +1,11 @@
+package com.shagtv.web.service
+
+import com.shagtv.web.datasource.BankDataSource
+import com.shagtv.web.model.Bank
+import org.springframework.stereotype.Service
+
+@Service
+class BankService (private val dataSource: BankDataSource) {
+
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+}
---
+++ src/test/kotlin/com/shagtv/web/service/BankServiceTest.kt
@@ -0,0 +1,22 @@
+package com.shagtv.web.service
+
+import com.shagtv.web.datasource.BankDataSource
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+internal class BankServiceTest {
+
+    private val dataSource: BankDataSource = mockk(relaxed = true)
+    private val bankService = BankService(dataSource)
+
+    @Test
+    fun `should call its data source to retrieve banks`() {
+        // when
+        val banks = bankService.getBanks()
+
+        // then
+        verify(exactly = 1) { dataSource.retrieveBanks() }
+    }
+}
